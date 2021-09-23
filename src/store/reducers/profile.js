c22f79b6cd6b07bcf8f0f0bfdf6f9325bbe61c67
MarcusSten <malmeloon@gmail.com>
2021-09-23 12:20:53 +0300

    add Lesson5

---
+++ src/store/reducers/profile.js
@@ -0,0 +1,27 @@
+import { CHANGE_IS_ONLINE, CHANGE_NAME } from "../actions/profile.js";
+
+const initialState = {
+    name: "Marcus",
+    age: 36,
+    isOnline: true
+}
+
+export default function profileReducer(state = initialState, action) {
+    switch(action.type) {
+        case CHANGE_NAME: {
+            return {
+                ...state,
+                name: action.payload.name
+                // showName: !state.showName
+            }
+        }
+        case CHANGE_IS_ONLINE: {
+            return {
+                ...state,
+                isOnline: action.payload.isOnline
+            }
+        }
+        default:
+            return state;
+    }
+}
