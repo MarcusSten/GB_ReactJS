bb0ad844850b6d758b50f566653192e04660fb18
MarcusSten <malmeloon@gmail.com>
2021-10-08 09:37:58 +0300

    add Lesson10

---
+++ src/store/reducers/messages.test.js
@@ -0,0 +1,18 @@
+import messagesReducer from "./messages";
+import { addMessage, ADD_MESSAGE } from "../actions/messages";
+import AUTHORS from "../../components/AddMsgForm/constants";
+
+describe("msgReducer", () => {
+    it("check state", () => {
+        const expect = {
+            messages: [],
+            request: {
+                status: ADD_MESSAGE,
+                error: null,
+            }
+        }
+    
+        const received = messagesReducer(undefined, addMessage());
+        expect(received).toEqual(expect);
+    });
+})
